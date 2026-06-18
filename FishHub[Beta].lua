447f46b67ab042c30468f528936f92cf7dc57152
KIETVN02202 <nhinlamgidungnhinnua@gmail.com>
2026-06-18 19:26:46 +0700

    Update FishHub[Beta].lua

--- FishHub[Beta].lua
+++ FishHub[Beta].lua
@@ -4,7 +4,7 @@ if gameId == 10449761463 then
 elseif gameId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KietVN02202/FishHub/refs/heads/main/MM2.lua"))()
 elseif gameId == 8412352352 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KietVN02202/FishHub/refs/heads/main/AOT.lua"))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KietVN02202/FishHub/refs/heads/main/AOT.lua"))()
 else
     print("wrong game")
 end
