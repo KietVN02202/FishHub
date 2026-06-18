097d1d52bc26d292312a90db3d76c9100da85d96
KIETVN02202 <nhinlamgidungnhinnua@gmail.com>
2026-06-18 14:35:53 +0700

    Create FishHub[Beta].lua

---
+++ FishHub[Beta].lua
@@ -0,0 +1,10 @@
+local gameId = game.PlaceId
+if gameId == 10449761463 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KietVN02202/FishHub/refs/heads/main/TSB.lua"))()
+elseif gameId == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KietVN02202/FishHub/refs/heads/main/MM2.lua"))()
+elseif gameId == 8412352352 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KietVN02202/FishHub/refs/heads/main/AOT.lua"))
+else
+    print("wrong game")
+end
